167b85f09e33e79d6beb29a320dc140137cdcd6b
ancientGlider <mmp57@mail.ru>
2022-01-14 11:54:09 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,45 +1,49 @@
 from flask import Flask, render_template    # сперва подключим модуль
-from flask_debugtoolbar import DebugToolbarExtension, toolbar
+from random import choice
 import data
 
 app = Flask(__name__)      # объявим экземпляр фласка
 
+def return_error():
+    pass
+
 @app.route('/')
-def main():
-    return render_template('index.html')
+def main_page():
+    def choice_tours():
+        res = {}
+        l = list(data.tours)
+        while len(res) < 6 and len(res) < len(l):
+            tour_number = choice(l)
+            res[tour_number] = data.tours[tour_number]
+        return res
+    return render_template('index.html', departures=data.departures, tours=choice_tours())
 
 @app.route('/departures/<departure>/')
 def show_departures(departure):
-    return render_template('departure.html')
+    if departure in data.departures:
+        return render_template('departure.html', departures=data.departures,
+                               tours={k: v for k, v in data.tours.items() if v["departure"] == departure},
+                               departure=departure)
+    else:
+        return render_template('oops.html', departures=data.departures)
 
 @app.route('/tours/<id>/')
 def show_tours(id):
-    return render_template('tour.html')
+    try:
+        id = int(id)
+    except Exception:
+        pass
+    if id in data.tours:
+        return render_template('tour.html', departures=data.departures, departure=data.tours[id]["departure"],
+                               tour=data.tours[id])
+    else:
+        return render_template('oops.html', departures=data.departures)
 
-@app.route('/data/')
-def show_data_all():
-    s_out = render_template('data_all.html')
-    for k, v in data.tours.items():
-        s_out += render_template('data_tour.html', country=v["country"], key=k, hotel=v["title"], price=v["price"], stars=v["stars"])
-    return s_out
+@app.route('/buy/<price>/')
+def show_purchase(price):
+    if price.isdigit():
+        return render_template('payment.html', departures=data.departures, price=price)
+    else:
+        return render_template('oops.html', departures=data.departures)
 
-@app.route('/data/departures/<departure>/')
-def show_data_departure(departure):
-    departure_value = departure.lower()
-    if departure_value not in data.departures:
-        return render_template('data_tours.html', direction=departure, not_found=" не найдены")
-    s_out = render_template('data_tours.html', direction=data.departures[departure_value])
-    for k, v in data.tours.items():
-        if v["departure"] == departure_value:
-            s_out += render_template('data_tour.html', country=v["country"], key=k, hotel=v["title"], price=v["price"], stars=v["stars"])
-    return s_out
-
-@app.route('/data/tours/<int:id>/')
-def show_data_hotel(id):
-    if id not in data.tours:
-        return render_template('data_tours.html', direction=id, not_found=" не найдены")
-    v = data.tours[id]
-    return render_template('data_hotel.html', country=v["country"], hotel=v["title"], price=v["price"], nights=v["nights"], description=v["description"])
-
-toolbar = DebugToolbarExtension(app)
-app.run(debug=True)    # ('0.0.0.0',8000,debug=True)    # запустим сервер на 8000 порту!
+app.run('0.0.0.0', 5001, debug=False)
